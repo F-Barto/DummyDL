0bc392c03005e949a61b01bd1095eb59d7492159
F-Barto <fbartoc@hotmail.fr>
2020-04-03 09:47:30 +0200

    m

--- tasks/run_simpleMNIST.sh
+++ tasks/run_simpleMNIST.sh
@@ -3,6 +3,7 @@
 CONFIG_DIR="../configs/"
 
 python ../mnist/mnist_trainer.py \
+--gpus '0' \
 --model_config_file $CONFIG_DIR"simplemnist.yml" \
 --model_config_profile $CONFIG_DIR"default" \
 --project_config_file $CONFIG_DIR"project.yml" \
