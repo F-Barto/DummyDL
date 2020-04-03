ce4dfee78c577d3cc8c5fd33caea5c5121cc788d
F-Barto <fbartoc@hotmail.fr>
2020-04-03 09:41:42 +0200

    working remotly have to push for every minor change

--- tasks/tests/test_config_utils.sh
+++ tasks/tests/test_config_utils.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 PARAMS_TO_TEST="batch_size channels metric learning_rate decay_rate"
 python ../../config_utils.py ./test.yml default $PARAMS_TO_TEST
+echo "---------------------------------"
+PARAMS_TO_TEST="batch_size additional_param"
+python ../../config_utils.py ./test.yml large $PARAMS_TO_TEST
