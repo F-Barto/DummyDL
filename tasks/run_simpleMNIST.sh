074b09ca1c894faf58c33edcffdb8bab6164917e
F-Barto <fbartoc@hotmail.fr>
2020-04-03 10:05:56 +0200

    correct config

--- tasks/run_simpleMNIST.sh
+++ tasks/run_simpleMNIST.sh
@@ -5,6 +5,6 @@ CONFIG_DIR="../configs/"
 python ../train.py \
 --gpus '0' \
 --model_config_file $CONFIG_DIR"simplemnist.yml" \
---model_config_profile $CONFIG_DIR"default" \
+--model_config_profile "default" \
 --project_config_file $CONFIG_DIR"project.yml" \
---project_config_profile $CONFIG_DIR"default"
+--project_config_profile "default"
